12a2844e90e49449beee7f12b4a3286c77fa9ffd
Eliot Rodriguez <mr.ear84@gmail.com>
2018-04-23 14:24:26 -0400

    the survey form now outputs to a variable that will be used to push via api also using event.preventDefault to stop the page from reloading

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,10 @@ module.exports = function(app){
 	app.get("/api/friends", function(req, res){
 		res.json(friendsData);
 	});
+
+	app.post("/api/friends", function(req, res){
+		console.log(req.body);
+	});
 };
 
 
