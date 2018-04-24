dc2c60243f4f2eabd9e1b05f8f3dcaa80eca2469
Eliot Rodriguez <mr.ear84@gmail.com>
2018-04-24 16:49:28 -0400

    trying to add css and styling

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+
 
 require("./app/routing/htmlRoutes.js")(app);
 require("./app/routing/apiRoutes.js")(app);
