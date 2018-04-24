d093d323b93b473732df0d2c0f162697c9c8460f
Eliot Rodriguez <mr.ear84@gmail.com>
2018-04-24 13:33:26 -0400

    added algorithm to calculate the differences between the current user survey answers and all potential friends. Also, returns the most compatible friend :-)

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,8 +12,40 @@ module.exports = function(app){
 		friendsData.push(req.body);
 		console.log("COOL!: " + req.body);
 
-		var count = friendsData.length;
-		res.send(count);
+	var count = friendsData.length;
+	var result = [];
+	var runningTotal = 0;
+		//For every friend in the list
+		for (var i = 0; i < count - 1; i++){
+			//check/calculate the difference between the user and each friend
+			for(var x = 0; x < 10; x++){
+				var diff = friendsData[i].scores[x] - friendsData[friendsData.length - 1].scores[x];
+				diff = Math.abs(diff);
+				runningTotal = runningTotal + diff;
+				console.log("friend score: " + friendsData[i].scores[x] + " user score: " + friendsData[friendsData.length - 1].scores[x] + " the difference: " + diff + " the running total: " + runningTotal)
+			}
+
+			//new array holds the difference for each friend in the list and the friend id
+			result.push([runningTotal, i]);
+			runningTotal = 0;	
+		}
+		
+		//find the friend with the least amount of difference via sorting multidimensional array
+		result = result.sort(sortFunction);
+
+		function sortFunction(a, b) {
+    		if (a[0] === b[0]) {
+       		 return 0;
+    		}
+   			else {
+        	return (a[0] < b[0]) ? -1 : 1;
+   			}
+		}
+
+	//Show Results
+	console.log("here are the results: " + result);	
+
+		res.send("According to our finder magic, your new friend is: " + friendsData[result[0][1]].name + " photo: " + friendsData[result[0][1]].photo);
 	});
 };
 
